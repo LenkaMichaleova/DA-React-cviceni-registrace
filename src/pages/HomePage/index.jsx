71152576ccbae415301246a584d11a061848904d
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-11 18:24:33 +0200

    vyřešena druhá část úkolu - nastavena defaultní hodnota země na Českou republiku

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,19 +1,21 @@
-import { use, useState } from 'react';
+import { useState, useEffect } from 'react';
 import './style.css';
 
 export const HomePage = () => {
   const [userName, setUserName] = useState("")
-  const [country, setCountry] = useState("Česká Republika")
+  const [country, setCountry] = useState("Česká republika")
 
   return (
     <div className="container">
       <form onSubmit={() => alert(`Uživatel "${userName}" ze země ${country} se chce registrovat.`)}>
+        
         <label>Uživatelské jméno: {` `}
           <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
         </label>
         {userName.length === 0 && <div className='error'>Uživatelské jméno je povinný údaj</div>}
+        
         <label>
-          Země původu:
+          Země původu: {` `}
           <select value={country} onChange={(e) => setCountry(e.target.value)}>
             <option value="Chorvatsko">Chorvatsko</option>
             <option value="Česká republika">Česká republika</option>
@@ -21,6 +23,7 @@ export const HomePage = () => {
             <option value="Slovenská republika">Slovenská republika</option>
           </select>
         </label>
+
         <button type='submit' disabled={userName.length === 0 ? true : false}>Registrovat</button>
       </form>
     </div>
