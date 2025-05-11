2c95594979fba31ed457109c7c531e575340ac2f
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-11 17:43:41 +0200

    vyřešeno

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,15 +3,25 @@ import './style.css';
 
 export const HomePage = () => {
   const [userName, setUserName] = useState("")
+  const [country, setCountry] = useState("Česká Republika")
 
   return (
     <div className="container">
-      <form onSubmit={() => alert(`Uživatel "${userName}" se chce přihlásit`)}>
+      <form onSubmit={() => alert(`Uživatel "${userName}" ze země ${country} se chce registrovat.`)}>
         <label>Uživatelské jméno: {` `}
           <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
         </label>
+        {userName.length === 0 && <div className='error'>Uživatelské jméno je povinný údaj</div>}
+        <label>
+          Země původu:
+          <select value={country} onChange={(e) => setCountry(e.target.value)}>
+            <option value="Chorvatsko">Chorvatsko</option>
+            <option value="Česká republika">Česká republika</option>
+            <option value="Polsko">Polsko</option>
+            <option value="Slovenská republika">Slovenská republika</option>
+          </select>
+        </label>
         <button type='submit' disabled={userName.length === 0 ? true : false}>Registrovat</button>
-        {userName.length === 0 && <div>Uživatelské jméno je povinný údaj</div>}
       </form>
     </div>
   );
